90135883313049ef396c469ac227a40bb2dd0218
retro-canvas <mahdigh@live.com>
2022-11-18 07:34:11 +0330

    added initial files. WIP

---
+++ sqlzoo.sql
@@ -0,0 +1,12 @@
+-- 1. Population
+SELECT population FROM world
+  WHERE name = 'Germany'
+
+-- 2. Scandinavia
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+-- 3. Just the right size
+SELECT name, area 
+FROM world
+  WHERE area BETWEEN 200000 AND 250000
